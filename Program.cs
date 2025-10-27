857a4d75a38aa5660b61cf0623e619df04b78eae
LuisJair <jair.escalante@phs.mx>
2025-10-27 15:40:08 -0600

    feat: create controller, context, mapping, model, dto,migration and repository to category

--- Program.cs
+++ Program.cs
@@ -1,17 +1,41 @@
+using ApiEcommerce.Repository;
+using ApiEcommerce.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+    new OpenApiInfo { Title = "Ecommerce API" , Version = "v1"});
+});
+
+// Connection with DataBase
+var connectionSql = builder.Configuration.GetConnectionString("ConexionSql");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionSql));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddOpenApi();
+// This method to enable controllers
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
+app.UseSwagger();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json","Ecommerce API");
+    });
 }
 
 app.UseHttpsRedirection();
+// This method map controllers
 app.MapControllers();
 
 
